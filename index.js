8bb2f6c7f8576484cac124f7184bd520d0143ab3
maram qais <maramqaiszebar@gmail.com>
2024-02-25 11:56:37 +0300

    Update functions

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-// Code your solution in this file!
 
-function distanceFromHqInBlocks(pickupLocation) {
-    return Math.abs(pickupLocation - 42); 
-  }
-  
-  function distanceFromHqInFeet(pickupLocation) {
-    const blocks = distanceFromHqInBlocks(pickupLocation); 
-    return blocks * 264;
-  }
-  
-  function distanceTravelledInFeet(startBlock, endBlock) {
-    const distance = Math.abs(endBlock - startBlock); 
-  }
-  
-  function calculatesFarePrice(startBlock, endBlock) {
-    const distance = distanceTravelledInFeet(startBlock, endBlock); 
-    let fare = 0;
-    
+function distanceFromHqInBlocks(block) {
+    return Math.abs(block - 42);
+}
+
+function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
     if (distance <= 400) {
-      fare = 0; 
+        return 0; 
     } else if (distance > 400 && distance <= 2000) {
-      fare = (distance - 400) * 0.02; 
+        return (distance - 400) * 0.02;
     } else if (distance > 2000 && distance <= 2500) {
-      fare = 25; 
-    } else {
-      return 'cannot travel that far'; 
+        return 25; 
+        return 'cannot travel that far';
     }
-    
-    return fare;
-  }
+}
+
+
+console.log(distanceFromHqInBlocks(34)); 
+console.log(distanceFromHqInFeet(34));
+console.log(distanceTravelledInFeet(38, 34)); 
+console.log(calculatesFarePrice(38, 34)); 
+console.log(calculatesFarePrice(24, 34)); 
+console.log(calculatesFarePrice(43, 23)); 
+console.log(calculatesFarePrice(3000, 34)); 
