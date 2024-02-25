ffd95b177464d09943be8ea0674142a547b2e15b
maram qais <maramqaiszebar@gmail.com>
2024-02-25 11:59:13 +0300

    Update functions2

--- index.js
+++ index.js
@@ -14,20 +14,21 @@ function distanceTravelledInFeet(start, destination) {
 function calculatesFarePrice(start, destination) {
     const distance = distanceTravelledInFeet(start, destination);
     if (distance <= 400) {
-        return 0; 
+        return 0; // Free sample
     } else if (distance > 400 && distance <= 2000) {
-        return (distance - 400) * 0.02;
+        return (distance - 400) * 0.02; 
     } else if (distance > 2000 && distance <= 2500) {
         return 25; 
-        return 'cannot travel that far';
+    } else {
+        return 'cannot travel that far'; 
     }
 }
 
 
 console.log(distanceFromHqInBlocks(34)); 
-console.log(distanceFromHqInFeet(34));
+console.log(distanceFromHqInFeet(34)); 
 console.log(distanceTravelledInFeet(38, 34)); 
 console.log(calculatesFarePrice(38, 34)); 
 console.log(calculatesFarePrice(24, 34)); 
-console.log(calculatesFarePrice(43, 23)); 
+console.log(calculatesFarePrice(43, 23));
 console.log(calculatesFarePrice(3000, 34)); 
